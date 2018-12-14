5508e3e881b360bab251575d54cd280443391717
Liu Shiqi <liushiqi17@mails.ucas.ac.cn>
2018-12-14 12:44:11 +0800

    Add % and minus.

--- src/4-3/getop.c
+++ src/4-3/getop.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <math.h>
+#include <ctype.h>
 #include "calc.h"
+
+double mod(double d1, double d2) {
+  return (double) ((int) d2 % (int) d1);
+}
 
 enum ErrorType getop(void) {
   int temp;
@@ -10,7 +15,11 @@ enum ErrorType getop(void) {
         return ERNUM;
       } else return SUCC;
     case '-':
-      if (push(-(pop() - pop())) == -1) {
+      if (isdigit(getch())) {
+        ungetch();
+        push(-get_double());
+        return SUCC;
+      } else if (push(-(pop() - pop())) == -1) {
         return ERNUM;
       } else return SUCC;
     case '*':
@@ -22,6 +31,10 @@ enum ErrorType getop(void) {
         return ERNUM;
       } else if (temp == -2) {
         return EDIV0;
+      } else return SUCC;
+    case '%':
+      if (push(mod(pop(), pop())) == -1) {
+        return ERNUM;
       } else return SUCC;
     case '\n': {
       double d = pop();
